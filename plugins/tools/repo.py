731ed9eaebb73a249b7a58126e52511e58551d46
arvind0252 <167284195+arvind0252@users.noreply.github.com>
2024-06-28 14:16:03 +0530

    Update repo.py

--- plugins/tools/repo.py
+++ plugins/tools/repo.py
@@ -22,18 +22,18 @@ async def repo(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "🥀 𝐑𝐞𝐩𝐨 🥀", url=f"https://github.com/BikashHalderNew")
+                        "🥀 𝐑𝐞𝐩𝐨 🥀", url=f"https://t.me/ROC_KY098")
             ],          
             [
                     InlineKeyboardButton(
-                        "🥀 𝐏𝐫𝐨𝐦𝐨𝐭𝐢𝐨𝐧 🥀", url=f"https://t.me/BgtPromote")
+                        "🥀 𝐏𝐫𝐨𝐦𝐨𝐭𝐢𝐨𝐧 🥀", url=f"https://t.me/ROC_KY098")
                 ],
                 [
                     InlineKeyboardButton(
-                        "🥀 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 🥀", url=f"https://t.me/Bgt_Chat"
+                        "🥀 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 🥀", url=f"https://t.me/ROC_KY098"
                     ),
                     InlineKeyboardButton(
-                        "🥀 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 🥀", url=f"https://t.me/BikashGadgetsTech")
+                        "🥀 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 🥀", url=f"https://t.me/ROC_KY098")
                 ]
             ]
         ),
